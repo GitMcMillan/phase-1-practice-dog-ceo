43ec1821ca7c1830c3cd811e3d1bf45a6aad9320
alexandermcmillan <mcmillancontact@protonmail.com>
2023-11-16 16:34:37 -0800

    updated lab unfinished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+document.addEventListener(`DOMContentLoaded`, getData())
+const get = document.getElementById("dog-image-container")
+
+function getData(e) {
+  //add images here: id="dog-image-container"
+  const images = document.querySelector("div")
+  console.log(images);
+
+  fetch(imgUrl)
+
+    .then((resp) => resp.json())
+    .then((data) => { console.log(data) })
+
+
+    .catch((error) => {
+      console.log("BAD THINGS", error)
+    })
+}
